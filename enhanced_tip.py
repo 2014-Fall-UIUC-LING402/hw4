744513ba61e64e7b07a6d9232deaed1711cf42c4
Lane Schwartz <dowobeha@gmail.com>
2014-11-10 14:00:31 -0600

    Added enhanced_tip.py

---
+++ enhanced_tip.py
@@ -0,0 +1,39 @@
+###############################################################################
+# Extra Credit Instructions:
+################
+#
+# Implement an enhanced tip calculator class (3 points)
+#   that inherits from the Tip class implemented in tip.py
+#
+# When you are done, your code must:
+#
+# 1. Be called EnhancedTip
+#
+# 2. Inherit from Tip
+#
+# 3. Have a constructor (__init__) that properly processes its parameters:
+#   * Correctly process parameter when bill is an integer
+#   * Correctly process parameter when bill is a float
+#   * Correctly process parameter when bill is a string
+#   * Correctly process parameter when percentage is an integer
+#   * Correctly process parameter when percentage is an float
+#   * Correctly process parameter when percentage is an string
+#
+# 3. Have an __str__ method that takes locale into account:
+#
+#   The following format is required as the return value of __str__:
+#      * "{}% tip:\t{}"
+#
+#   The first slot in the above string should contain the tip percentage.
+#   The second slot in the above string should contain the calculated tip,
+#      and the tip must be formatted properly according to whatever locale
+#      Python is configured to use.
+# 
+###############################################################################
+###############################################################################
+
+
+# Implementing this file is optional extra credit
+
+
+
---
+++ run_unit_test_for_tip_extra_credit.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Run the unit test for enhanced_tip.py
+/home/lanes/Homeworks/hw4.unit-tests/tip_test_extra_credit $(pwd)
